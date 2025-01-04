3baa6508843a11626d160a0bbfb7118224715947
ThanuraSilva <thanurasilva1989@gmail.com>
2025-01-04 12:31:07 +0530

    Project init

---
+++ src/main/java/lk/ijse/cmjd108/conceptDemo/ConceptDemoApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd108.conceptDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptDemoApplication.class, args);
+	}
+
+}
